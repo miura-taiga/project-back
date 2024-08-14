f9e72a643f61e5d915142fdc9ed294ed8489c4cc
三浦大河 <taigav8@gmail.com>
2024-08-14 23:09:47 +0900

    トークン作成時にCookieに保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,21 +4,24 @@ class ApplicationController < ActionController::API
   protected
 
   def authenticate_request
-    header = request.headers['Authorization']
-    header = header.split(' ').last if header
-    begin
-      @decoded = JwtService.decode(header)
-      if @decoded['provider'] == 'guest'
-        @current_user = User.find(@decoded['user_id'])
-      else
-        user_auth = UserAuthentication.find_by(uid: @decoded['google_user_id'], provider: @decoded['provider'])
-        @current_user = user_auth.user if user_auth
+    token = cookies.signed[:auth_token]
+    if token
+      begin
+        @decoded = JwtService.decode(token)
+        if @decoded['provider'] == 'guest'
+          @current_user = User.find(@decoded['user_id'])
+        else
+          user_auth = UserAuthentication.find_by(uid: @decoded['google_user_id'], provider: @decoded['provider'])
+          @current_user = user_auth.user if user_auth
+        end
+        Rails.logger.info(@current_user)
+        raise ActiveRecord::RecordNotFound, 'User not found' unless @current_user
+      rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+        Rails.logger.error "認証エラー: #{e.message}"
+        render json: { redirect_url: "#{ENV['NEXT_PUBLIC_API_URL']}/pages/login" }, status: :unauthorized
       end
-      Rails.logger.info(@current_user)
-      raise ActiveRecord::RecordNotFound, 'User not found' unless @current_user
-    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
-      Rails.logger.error "認証エラー: #{e.message}"
-      render json: { errors: e.message }, status: :unauthorized
+    else
+      render json: { redirect_url: "#{ENV['NEXT_PUBLIC_API_URL']}/pages/login" }, status: :unauthorized
     end
   end
 end
